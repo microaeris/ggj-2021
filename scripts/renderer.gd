ff52081f28176a25ffe96d5e69ce099c6dc304e1
microaeris <alicewang54@gmail.com>
2021-01-29 00:59:49 -0800

    Can write to screen buffer

--- scripts/renderer.gd
+++ scripts/renderer.gd
@@ -31,12 +31,66 @@ const CHAR_DIAMOND: String = "\ue15a"
 ## Locals
 
 onready var Text = $Text
-
+var screen_buffer: Array = []  # 2D Array of strings to write to the text box
 
 ## Builtin Functions
 
 func _ready():
-	var test = CHAR_AT_INV + CHAR_AT + CHAR_UP_ARROW \
-		+CHAR_BOT_BAR+CHAR_TOP_BAR+CHAR_LEFT_BAR+CHAR_RIGHT_BAR+CHAR_DIAG+CHAR_ASTERICKS+CHAR_EXCLAMATION+CHAR_QUESTION+CHAR_DIAG_STRIPES \
-		+CHAR_X+ CHAR_HALF_DITHER+CHAR_HEART+CHAR_CIRCLE+CHAR_FILLED_CIRCLE+CHAR_DIAMOND
-	Text.set_text(test)
+	_clear_screen_buffer()
+	var test: Array = []
+	test.append([CHAR_CORNER_BOT_LEFT, CHAR_FILLED_CIRCLE, CHAR_DIAG])
+	test.append([CHAR_ASTERICKS])
+	_copy_into_screen_buffer(test, Vector2(10,10))
+	_update_screen()
+
+##
+
+func _clear_screen_buffer() -> void:
+	for x in range(SCREEN_CHAR_HEIGHT):
+		var col = []
+		col.resize(SCREEN_CHAR_WIDTH)
+		screen_buffer.append(col)
+
+
+func _update_screen() -> void:
+	"""
+	Write out screen_buffer to the RichTextLabel.
+	"""
+	assert(len(screen_buffer) <= SCREEN_CHAR_HEIGHT)
+
+	Text.set_text("")
+	for row in screen_buffer:
+		assert(len(row) <= SCREEN_CHAR_WIDTH)
+		for element in row:
+			if element == null:			
+				Text.text += " "
+			else:
+				Text.text += element
+		Text.text += "\n"
+	_clear_screen_buffer()
+
+
+func is_valid_screen_coord(point: Vector2) -> bool:
+	return (point.y < SCREEN_CHAR_HEIGHT) and \
+		(point.x < SCREEN_CHAR_WIDTH)
+
+
+func _copy_into_screen_buffer(src: Array, dest: Vector2) -> bool:
+	"""
+	Args:
+		dest: the 2D coordinates in screen_buffer where we will copy the src.
+			The dest points to the _top left_ of where the src is copied to.
+		src: 2D Array of strings. If any element of the array is null, 
+			then do not copy that character. 
+			If src cannot fit into the screen buffer, it will be cut off.
+			Writing a " " (space) clears the existing character.
+	"""
+	if not is_valid_screen_coord(dest):
+		return false
+	
+	for y in range(len(src)):
+		for x in range(len(src[y])):
+			if src[y][x] != null:
+				screen_buffer[dest.y + y][dest.x + x] = src[y][x]
+
+	return true
