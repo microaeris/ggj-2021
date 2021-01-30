76c56c16eba6c9f058dae0a33c7b42bebf171584
microaeris <alicewang54@gmail.com>
2021-01-30 00:12:03 -0800

    write edge calculation functions and unit tests

--- scripts/renderer.gd
+++ scripts/renderer.gd
@@ -63,21 +63,126 @@ const VOXEL_ADD_TOP: Array = [
 	[null, " ", " ", " "],
 ]
 
+const LEFT_L_CORNER: Array = [
+	[CHAR_CORNER_BOT_RIGHT],
+	[null, CHAR_DIAG],
+]
+
 ## Locals
 
 onready var Text = $Text
 var screen_buffer: Array = []  # 2D Array of strings to write to the text box
+
+# Dummy test world
+#	z
+#	^
+#	|
+#	|
+#	+-----> x
+#	 \
+#	  \
+#	   v
+#	     y
+var world: Array = [  # 3D array of bools. If true, it means a vowel exists there.
+	[
+		[0, 1, 1, 1],
+		[1, 1, 0, 1],
+		[1, 1, 0, 1],
+		[1, 1, 0, 1],
+	],
+	[
+		[0, 0, 0, 1],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	],
+	[
+		[0, 0, 0, 1],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+		[0, 0, 0, 0],
+	],
+]
 
 ## Builtin Functions
 
 func _ready():
 	_clear_screen_buffer()
 	var pos: Vector2 = Vector2(32,20)
-	_draw_1x1_voxel(pos)
-	_draw_row(5, pos)
+	var world_pos = Vector3(3, 0, 0)
 	
-	_draw_col(5, pos)
-	_update_screen()
+#	_draw_1x1_voxel(pos)
+#	_draw_row(3, pos, world_pos)
+#	_draw_col(1, pos, world_pos)
+#	_update_screen()
+
+#	assert(is_there_block_above(world_pos))
+#	world_pos.x -= 1
+#	assert(is_there_block_above(world_pos) == false)
+#	assert(is_there_block_below(world_pos) == false)
+#	world_pos = Vector3(3, 0, 0)
+#	assert(is_there_block_below(world_pos) == false)
+#	world_pos.z += 1
+#	assert(is_there_block_below(world_pos) == true)
+#	world_pos = Vector3(0, 0, 1)
+#	assert(is_there_block_below(world_pos) == false)
+#	world_pos = Vector3(1, 0, 0)
+#	assert(is_there_block_left(world_pos) == false)
+#	world_pos = Vector3(2, 0, 0)
+#	assert(is_there_block_left(world_pos) == true)
+#	world_pos = Vector3(1, 0, 0)
+#	assert(is_there_block_right(world_pos) == true)
+#	world_pos = Vector3(1, 0, 1)
+#	assert(is_there_block_right(world_pos) == false)
+#	world_pos = Vector3(0, 1, 0)
+#	assert(is_there_block_infront(world_pos) == true)
+#	world_pos = Vector3(0, 0, 1)
+#	assert(is_there_block_infront(world_pos) == false)
+#	world_pos = Vector3(0, 2, 0)
+#	assert(is_there_block_behind(world_pos) == true)
+#	world_pos = Vector3(0, 1, 0)
+#	assert(is_there_block_behind(world_pos) == false)
+
+#	assert(is_valid_world_pos(Vector3(0, 0, 0)) == true)
+#	assert(is_valid_world_pos(Vector3(1, 2, 1)) == true)
+#	assert(is_valid_world_pos(Vector3(3, 3, 1)) == true)
+#	assert(is_valid_world_pos(Vector3(4, 3, 1)) == false)
+#	assert(is_valid_world_pos(Vector3(3, 4, 1)) == false)
+#	assert(is_valid_world_pos(Vector3(3, 3, 2)) == false)
+#	assert(is_valid_world_pos(Vector3(-1, 3, 1)) == false)
+
+#	assert(_count_y_edge(Vector3(0, 3, 0)) == 3, "Expected 3, was " + str(_count_y_edge(Vector3(0, 3, 0))))
+#	var test_pos = Vector3(3, 0, 1)
+#	assert(_count_y_edge(test_pos) == 1, "Expected 1, was " + str(_count_y_edge(test_pos)))
+#	test_pos = Vector3(3, 3, 0)
+#	var expected = 3
+#	assert(_count_y_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_y_edge(test_pos)))
+#	test_pos = Vector3(3, 0, 0)
+#	expected = 0
+#	assert(_count_y_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_y_edge(test_pos)))
+
+#   # Tests x edge
+#	var test_pos = Vector3(0, 3, 0)
+#	var expected = 2
+#	assert(_count_x_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_x_edge(test_pos)))
+#	test_pos = Vector3(3, 0, 1)
+#	expected = 1
+#	assert(_count_x_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_x_edge(test_pos)))
+#	test_pos = Vector3(3, 0, 0)
+#	expected = 0
+#	assert(_count_x_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_x_edge(test_pos)))
+
+#	# Tests z edge
+#	var test_pos = Vector3(3, 0, 2)
+#	var expected = 2
+#	assert(_count_z_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_z_edge(test_pos)))
+#	test_pos = Vector3(1, 3, 0)
+#	expected = 0
+#	assert(_count_z_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_z_edge(test_pos)))
+#	test_pos = Vector3(0, 3, 0)
+#	expected = 1
+#	assert(_count_z_edge(test_pos) == expected, "Expected " + str(expected) + ", was " + str(_count_z_edge(test_pos)))
+
 
 ##
 
@@ -138,42 +243,47 @@ func _draw_1x1_voxel(pos: Vector2) -> bool:
 	return true
 
 
-func _draw_row(num_voxels: int, pos: Vector2) -> bool:
+func _draw_row(num_voxels: int, pos: Vector2, world_pos: Vector3) -> bool:
 	"""
 	Assumes we're appending onto existing voxels
 	Args
 		num_voxels: number of voxels to draw in a row.
 		pos: position of existing voxel we're appending to in the screen_buffer.
+		world_pos: position of the existing block in the 3D world
 	"""
 	# FIXME - handle draw calls that end outside of screen buffer or start outside of screen buffer.
 	
 	for i in range(num_voxels):
 		pos.x -= VOXEL_WIDTH
-		_add_voxel_left(pos)
+		world_pos.x -= 1
+		_add_voxel_left(pos, world_pos)
 	return true
 
 
-func _draw_col(num_voxels: int, pos: Vector2) -> bool:
+func _draw_col(num_voxels: int, pos: Vector2, world_pos: Vector3) -> bool:
 	"""
 	Assumes we're appending onto existing voxels
 	Args
 		num_voxels: number of voxels to draw in a row.
 		pos: position of existing voxel we're appending to in the screen_buffer.
+		world_pos: position of the existing block in the 3D world
 	"""
 	# FIXME - handle draw calls that end outside of screen buffer or start outside of screen buffer.
 	
 	for i in range(num_voxels):
 		pos.y -= VOXEL_HEIGHT
-		_add_voxel_top(pos)
+		world_pos.z += 1
+		_add_voxel_top(pos, world_pos)
 	return true
 
 
-func _add_voxel_left(pos:Vector2) -> bool:
+func _add_voxel_left(pos:Vector2, world_pos: Vector3) -> bool:
 	"""
 	Draws a new voxel to the left assuming this voxel is the bottom layer.
 	Increases the interior line length and moves it over.
 	Args
 		pos: position in screen buffer to draw the new voxel.
+		world_pos: position of the new block in the 3D world
 	"""
 #   # Check if the voxel to our left matches the shape we expect...
 #   var pos_right: Vector2 = pos + Vector2(3, 0)
@@ -194,7 +304,6 @@ func _add_voxel_left(pos:Vector2) -> bool:
 	
 	# Draw new interior lines. Should draw two more than old value. 
 	num_interior_horiz_lines += 1
-	print(num_interior_horiz_lines)
 	var offset_pos_to_interior_horiz_line: Vector2 = Vector2(3, 2)  # Constant
 	screen_pos = pos + offset_pos_to_interior_horiz_line
 	for i in range(num_interior_horiz_lines):
@@ -203,16 +312,155 @@ func _add_voxel_left(pos:Vector2) -> bool:
 
 	return true
 
+## 
+# Dummy world functions that will be replaced later
 
-func _add_voxel_top(pos:Vector2) -> bool:
+# All these functions assume world_pos is a valid point in the world.
+
+func is_there_block_above(world_pos: Vector3) -> bool:
+	if (world_pos.z + 1) >= len(world):
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z + 1][world_pos.y][world_pos.x] > 0
+
+
+func is_there_block_below(world_pos: Vector3) -> bool:
+	if (world_pos.z - 1) < 0:
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z - 1][world_pos.y][world_pos.x] > 0
+
+
+func is_there_block_left(world_pos: Vector3) -> bool:
+	if (world_pos.x - 1) < 0:
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z][world_pos.y][world_pos.x - 1] > 0
+
+
+func is_there_block_right(world_pos: Vector3) -> bool:
+	if (world_pos.x + 1) >= len(world[0][0]):
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z][world_pos.y][world_pos.x + 1] > 0
+
+
+func is_there_block_behind(world_pos: Vector3) -> bool:
+	if (world_pos.y - 1) < 0:
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z][world_pos.y - 1][world_pos.x] > 0
+
+
+func is_there_block_infront(world_pos: Vector3) -> bool:
+	if (world_pos.y + 1) >= len(world[0]):
+		# Out of bounds index. No blocks exist there.
+		return false
+	return world[world_pos.z][world_pos.y + 1][world_pos.x] > 0
+
+
+func is_valid_world_pos(world_pos: Vector3) -> bool:
+	return ((world_pos.z >= 0) && (world_pos.z < len(world))) \
+		&& ((world_pos.y >= 0) && (world_pos.y < len(world[0]))) \
+		&& ((world_pos.x >= 0) && (world_pos.x < len(world[0][0])))
+
+##
+
+func _count_y_edge(world_pos: Vector3) -> int:
+	"""
+	Returns the number of continous blocks in the y direction starting at 
+	world_pos. This function assumes there are no blocks in FRONT of world_pos. 
+	Notes: y defined the back to front dimension.
+	""" 
+	var edge_len = 0
+	var found_new_plane = false
+	var cur_pos = world_pos
+	while not found_new_plane:
+		if world[cur_pos.z][cur_pos.y][cur_pos.x] == 0:
+			break
+		
+		found_new_plane = found_new_plane or is_there_block_above(cur_pos)
+		found_new_plane = found_new_plane or is_there_block_left(cur_pos)
+		if found_new_plane:
+			break
+		else:
+			edge_len += 1
+			cur_pos.y -= 1
+
+		if not is_valid_world_pos(cur_pos):
+			break
+		
+	return edge_len
+		
+
+func _count_x_edge(world_pos: Vector3) -> int:
+	"""
+	Returns the number of continous blocks in the x direction starting at 
+	world_pos. This function assumes there are no blocks to the left of world_pos. 
+	Notes: x defined the left to right dimension.
+	""" 
+	var edge_len = 0
+	var found_new_plane = false
+	var cur_pos = world_pos
+	while not found_new_plane:
+		if world[cur_pos.z][cur_pos.y][cur_pos.x] == 0:
+			break
+		
+		found_new_plane = found_new_plane or is_there_block_above(cur_pos)
+		found_new_plane = found_new_plane or is_there_block_infront(cur_pos)
+		if found_new_plane:
+			break
+		else:
+			edge_len += 1
+			cur_pos.x += 1
+
+		if not is_valid_world_pos(cur_pos):
+			break
+		
+	return edge_len
+
+
+func _count_z_edge(world_pos: Vector3) -> int:
+	"""
+	Returns the number of continous blocks in the z direction starting at 
+	world_pos. This function assumes there are no blocks above world_pos. 
+	Notes: z defined the up to down dimension.
+	""" 
+	var edge_len = 0
+	var found_new_plane = false
+	var cur_pos = world_pos
+	while not found_new_plane:
+		if world[cur_pos.z][cur_pos.y][cur_pos.x] == 0:
+			break
+		
+		found_new_plane = found_new_plane or is_there_block_left(cur_pos)
+		found_new_plane = found_new_plane or is_there_block_infront(cur_pos)
+		if found_new_plane:
+			break
+		else:
+			edge_len += 1
+			cur_pos.z -= 1
+
+		if not is_valid_world_pos(cur_pos):
+			break
+		
+	return edge_len
+
+
+func _add_voxel_top(pos:Vector2, world_pos: Vector3) -> bool:
 	"""
 	Draws a new voxel on top of an existing voxel. Assumes new voxel to be added
 	is the top most layer.
 	Increases the interior line length and moves it over.
 	Args
 		pos: position in screen buffer to draw the new voxel.
+		world_pos: position of the new block in the 3D world
 	"""
 	# FIXME - need to create the array to represent the left part of a 1x1 cube.
+	
+	if world[world_pos.z - 1][world_pos.y][world_pos.x] == 0:
+		# Error out since this block doesn't exist.
+		assert("We assume we're ADDING on top of an existing block.")
 	
 	_copy_into_screen_buffer(VOXEL_ADD_TOP, pos) 
 
@@ -232,5 +480,13 @@ func _add_voxel_top(pos:Vector2) -> bool:
 	for i in range(num_interior_vert_lines):
 		screen_buffer[screen_pos.y][screen_pos.x] = CHAR_LEFT_BAR
 		screen_pos.y += 1
+
+	## Special cases!
+	
+	# If block exists below and to the left.
+	if world[world_pos.z - 1][world_pos.y][world_pos.x - 1] > 0:
+		var offset_pos_to_corner: Vector2 = Vector2(0, 3)  # Constant]
+		screen_pos = pos + offset_pos_to_corner
+		_copy_into_screen_buffer(LEFT_L_CORNER, screen_pos)
 
 	return true
