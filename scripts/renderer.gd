09c43b70c890234733fbccbb1c556ab4498bff07
microaeris <alicewang54@gmail.com>
2021-01-29 21:11:32 -0800

    Can draw single rows and cols

--- scripts/renderer.gd
+++ scripts/renderer.gd
@@ -1,9 +1,17 @@
 extends Control
 
+## Notes
+
+# Null values in unicode arrays denote a transparent block
+# Spaces means something similar. but it will override buffers they are copied to.
+
 ## Constants
 
 const SCREEN_CHAR_WIDTH: int  = 40 
 const SCREEN_CHAR_HEIGHT: int  = 25
+
+const VOXEL_WIDTH: int = 3
+const VOXEL_HEIGHT: int = 3
 
 const CHAR_AT_INV: String = "\ue680"
 const CHAR_AT: String = "\ue600"
@@ -38,6 +46,23 @@ const VOXEL_1x1: Array = [
 	[" ", CHAR_DIAG, CHAR_BOT_BAR, CHAR_BOT_BAR, CHAR_CORNER_BOT_RIGHT],
 ]
 
+const VOXEL_ADD_LEFT: Array = [
+	[" ", CHAR_BOT_BAR, CHAR_BOT_BAR, CHAR_BOT_BAR],
+	[CHAR_RIGHT_BAR, CHAR_DIAG, " ", " ", " "],
+	[CHAR_RIGHT_BAR, " ", CHAR_CORNER_TOP_LEFT, " ", " ", " "],
+	[CHAR_RIGHT_BAR, " ", " ", " ", " ", " "],
+	[" ", CHAR_DIAG, CHAR_BOT_BAR, CHAR_BOT_BAR, CHAR_BOT_BAR],
+]
+
+const VOXEL_ADD_TOP: Array = [
+	[" ", CHAR_BOT_BAR, CHAR_BOT_BAR, CHAR_BOT_BAR],
+	[CHAR_RIGHT_BAR, CHAR_DIAG, " ", " ", CHAR_DIAG],
+	[CHAR_RIGHT_BAR, " ", CHAR_CORNER_TOP_LEFT, " ", CHAR_RIGHT_BAR],
+	[CHAR_RIGHT_BAR, " ", " ", " ", CHAR_RIGHT_BAR],
+	[null, " ", " ", " ", CHAR_RIGHT_BAR],
+	[null, " ", " ", " "],
+]
+
 ## Locals
 
 onready var Text = $Text
@@ -47,7 +72,11 @@ var screen_buffer: Array = []  # 2D Array of strings to write to the text box
 
 func _ready():
 	_clear_screen_buffer()
-	_copy_into_screen_buffer(VOXEL_1x1, Vector2(10,10))
+	var pos: Vector2 = Vector2(32,20)
+	_draw_1x1_voxel(pos)
+	_draw_row(5, pos)
+	
+	_draw_col(5, pos)
 	_update_screen()
 
 ##
@@ -69,7 +98,7 @@ func _update_screen() -> void:
 	for row in screen_buffer:
 		assert(len(row) <= SCREEN_CHAR_WIDTH)
 		for element in row:
-			if element == null:			
+			if element == null:         
 				Text.text += " "
 			else:
 				Text.text += element
@@ -99,5 +128,109 @@ func _copy_into_screen_buffer(src: Array, dest: Vector2) -> bool:
 		for x in range(len(src[y])):
 			if src[y][x] != null:
 				screen_buffer[dest.y + y][dest.x + x] = src[y][x]
+
+	return true
+
+
+func _draw_1x1_voxel(pos: Vector2) -> bool:
+	# FIXME - check that pos is in bounds and handle drawing part of a cube
+	_copy_into_screen_buffer(VOXEL_1x1, pos)
+	return true
+
+
+func _draw_row(num_voxels: int, pos: Vector2) -> bool:
+	"""
+	Assumes we're appending onto existing voxels
+	Args
+		num_voxels: number of voxels to draw in a row.
+		pos: position of existing voxel we're appending to in the screen_buffer.
+	"""
+	# FIXME - handle draw calls that end outside of screen buffer or start outside of screen buffer.
+	
+	for i in range(num_voxels):
+		pos.x -= VOXEL_WIDTH
+		_add_voxel_left(pos)
+	return true
+
+
+func _draw_col(num_voxels: int, pos: Vector2) -> bool:
+	"""
+	Assumes we're appending onto existing voxels
+	Args
+		num_voxels: number of voxels to draw in a row.
+		pos: position of existing voxel we're appending to in the screen_buffer.
+	"""
+	# FIXME - handle draw calls that end outside of screen buffer or start outside of screen buffer.
+	
+	for i in range(num_voxels):
+		pos.y -= VOXEL_HEIGHT
+		_add_voxel_top(pos)
+	return true
+
+
+func _add_voxel_left(pos:Vector2) -> bool:
+	"""
+	Draws a new voxel to the left assuming this voxel is the bottom layer.
+	Increases the interior line length and moves it over.
+	Args
+		pos: position in screen buffer to draw the new voxel.
+	"""
+#   # Check if the voxel to our left matches the shape we expect...
+#   var pos_right: Vector2 = pos + Vector2(3, 0)
+#   if not _check_screen_buffer_equal(VOXEL_ADD_LEFT, pos_right):
+#       return false
+	# FIXME - need to create the array to represent the left part of a 1x1 cube.
+	
+	_copy_into_screen_buffer(VOXEL_ADD_LEFT, pos)
+	
+	# Count the current length of interior line and erase them.
+	var num_interior_horiz_lines = 0
+	var offset_pos_to_old_interior_horiz_line: Vector2 = Vector2(6, 2)  # Constant
+	var screen_pos: Vector2 = pos + offset_pos_to_old_interior_horiz_line
+	while (screen_buffer[screen_pos.y][screen_pos.x] == CHAR_TOP_BAR):
+		screen_buffer[screen_pos.y][screen_pos.x] = " "
+		num_interior_horiz_lines += 1
+		screen_pos.x += 1
+	
+	# Draw new interior lines. Should draw two more than old value. 
+	num_interior_horiz_lines += 1
+	print(num_interior_horiz_lines)
+	var offset_pos_to_interior_horiz_line: Vector2 = Vector2(3, 2)  # Constant
+	screen_pos = pos + offset_pos_to_interior_horiz_line
+	for i in range(num_interior_horiz_lines):
+		screen_buffer[screen_pos.y][screen_pos.x] = CHAR_TOP_BAR
+		screen_pos.x += 1
+
+	return true
+
+
+func _add_voxel_top(pos:Vector2) -> bool:
+	"""
+	Draws a new voxel on top of an existing voxel. Assumes new voxel to be added
+	is the top most layer.
+	Increases the interior line length and moves it over.
+	Args
+		pos: position in screen buffer to draw the new voxel.
+	"""
+	# FIXME - need to create the array to represent the left part of a 1x1 cube.
+	
+	_copy_into_screen_buffer(VOXEL_ADD_TOP, pos) 
+
+	# Count the current length of interior line and erase them.
+	var num_interior_vert_lines = 0
+	var offset_pos_to_old_interior_vert_line: Vector2 = Vector2(2, 6)  # Constant
+	var screen_pos: Vector2 = pos + offset_pos_to_old_interior_vert_line
+	while (screen_buffer[screen_pos.y][screen_pos.x] == CHAR_LEFT_BAR):
+		screen_buffer[screen_pos.y][screen_pos.x] = " "
+		num_interior_vert_lines += 1
+		screen_pos.y += 1
+	
+	# Draw new interior lines. Should draw two more than old value. 
+	num_interior_vert_lines += 1
+	var offset_pos_to_interior_vert_line: Vector2 = Vector2(2, 3)  # Constant
+	screen_pos = pos + offset_pos_to_interior_vert_line
+	for i in range(num_interior_vert_lines):
+		screen_buffer[screen_pos.y][screen_pos.x] = CHAR_LEFT_BAR
+		screen_pos.y += 1
 
 	return true
