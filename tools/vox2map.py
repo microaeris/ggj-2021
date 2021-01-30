be3299cf015c0d8b73100373c1e15252411baecc
Jordan Mecom <jmecom@wustl.edu>
2021-01-30 00:05:32 -0800

    Add vox2map.py and an initial test map

---
+++ tools/vox2map.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+import struct
+from sys import argv
+
+DEBUG_MODE = False
+
+def debug(*args):
+    if DEBUG_MODE:
+        print(args)
+
+class VoxelMap(object):
+    def __init__(self, x_dim, y_dim, z_dim):
+        self.x_dim = x_dim
+        self.y_dim = y_dim
+        self.z_dim = z_dim
+
+        self.map = [[[0 for k in xrange(z_dim)] \
+            for j in xrange(y_dim)] for i in xrange(x_dim)]
+
+    def get(self, x, y, z):
+        return self.map[z][y][x]
+
+    def set(self, x, y, z, i):
+        self.map[z][y][x] = i
+
+
+def int32(buffer, offset):
+    return struct.unpack("I", buffer[offset:offset+4])[0]
+
+def parse_vox(buffer):
+    if len(buffer) > 4 and buffer[0:4] != "VOX ":
+        print("File is not valid .vox")
+        return False
+
+    # 1) Parse SIZE chunk to get dimensions
+    size_chunk = buffer.find("SIZE")
+    if size_chunk == -1:
+        print("Could not find size chunk")
+        return False
+
+    x_dim = ord(buffer[size_chunk + 12 + 4 * 0])
+    y_dim = ord(buffer[size_chunk + 12 + 4 * 1])
+    z_dim = ord(buffer[size_chunk + 12 + 4 * 2])
+
+    debug(x_dim, y_dim, z_dim)
+
+    # 2) Parse XYZI chunk to get voxel data
+    xyzi_chunk = buffer.find("XYZI")
+    if xyzi_chunk == -1:
+        print("Could not find xyzi chunk")
+        return False
+
+    voxel_count = int32(buffer, xyzi_chunk + 12)
+    voxels_offset = xyzi_chunk + 12 + 4
+
+    debug(xyzi_chunk, voxel_count)
+
+    # 3) Build up our in-memory representation of the .vox
+    map = VoxelMap(x_dim, y_dim, z_dim)
+    debug("offset @ %d" % voxels_offset)
+    debug("start with %d" % ord(buffer[voxels_offset]))
+
+    k = 0
+    while k < voxel_count * 4:
+        x, y, z, i = buffer[voxels_offset + k], buffer[voxels_offset + k+1], \
+            buffer[voxels_offset + k+2], buffer[voxels_offset + k+3]
+        x, y, z, i = ord(x), ord(y), ord(z), ord(i)
+        k += 4
+
+        map.set(x, y, z, i)
+        debug(map.get(x, y, z))
+
+    print(map.map)
+
+    return True
+
+def main():
+    def usage():
+        print("cough up an input file path, bud")
+        exit(1)
+
+    argc = len(argv)
+    if argc != 2:
+        usage()
+
+    vox_path = argv[1]
+
+    vox_buffer = open(vox_path, "rb").read()
+
+    if not parse_vox(vox_buffer):
+        print("Failed to parse .vox file")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
