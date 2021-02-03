0b8f5bc66e798b876e8384e09e9e262d8f31389b
microaeris <alicewang54@gmail.com>
2021-02-03 00:50:32 -0800

    can jump and fall

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,13 +6,16 @@ extends Control
 
 ## Consts
 
-const BUTTON_WAIT_MS: float = .4
+const BUTTON_WAIT_MS: float = .25
+const MAX_JUMP_HEIGHT: int = 4
 
 ## Locals
 
 var player_pos: Vector3 = Vector3(0,0,0)  # In char map coords
 var player_grabbing: bool = false
-var player_jumping: bool = true
+var player_jumping: bool = false
+var player_falling: bool = false
+var player_cur_jump_height:int = 0
 var up_pressed: bool = false
 var down_pressed: bool = false
 var left_pressed: bool = false
@@ -31,7 +34,7 @@ func _ready():
 
 
 func _process(delta):
-	# TODO Animate jump
+	handle_jump(false)
 
 	if up_pressed or down_pressed or left_pressed or right_pressed:
 		press_delta += delta
@@ -74,7 +77,7 @@ func _input(event):
 		press_delta = 0.0
 	elif event.is_action_pressed("grab"):
 		handled = true
-		if not player_jumping:
+		if not player_jumping and not player_falling:
 			player_grabbing = true
 	elif event.is_action_released("grab"):
 		handled = true
@@ -82,8 +85,10 @@ func _input(event):
 		press_delta = 0.0
 	elif event.is_action_pressed("jump"):
 		handled = true
-		if not player_grabbing:
+		# This line prevents double jumping and chaining jumps.
+		if not player_grabbing and not player_jumping and not player_falling:
 			player_jumping = true
+			handle_jump(true)
 	
 	if handled:
 		get_tree().set_input_as_handled()
@@ -105,6 +110,33 @@ func handle_wasd_input() -> void:
 		renderer_node.clear_screen_buffer()
 		renderer_node.draw_map()
 		renderer_node.update_screen()
+
+
+func handle_jump(start_new_jump: bool) -> void:
+	var new_pos: Vector3 = player_pos
+	if start_new_jump:
+		player_cur_jump_height = 1
+		new_pos.z += 1
+	elif player_jumping:
+		if player_cur_jump_height < MAX_JUMP_HEIGHT:
+			player_cur_jump_height += 1
+			new_pos.z += 1
+		elif player_cur_jump_height == MAX_JUMP_HEIGHT:
+			player_jumping = false
+			player_falling = true
+			player_cur_jump_height = 0
+			# Pause at top frame for 1 tick, so don't do `new_pos.z -= 1`.
+	elif player_falling:
+		new_pos.z -= 1
+
+	if calc_player_map_collision(new_pos) == new_pos:
+		set_pos(new_pos)
+		renderer_node.clear_screen_buffer()
+		renderer_node.draw_map()
+		renderer_node.update_screen()
+	else:
+		# Collided with map and so now player is done falling.
+		player_falling = false
 
 
 func set_pos(pos: Vector3) -> bool:
